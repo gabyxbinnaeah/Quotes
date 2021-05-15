5f8ea3f50f1fcf33053936d7a4456038d7f30d2c
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-05-15 15:18:31 +0300

    imports HostListener and used it to define the events that will be initiated by user actions

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Host, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
@@ -6,6 +6,16 @@ import { Directive, ElementRef } from '@angular/core';
 export class HighlightDirective {
 
   constructor(private elem:ElementRef) { } //elementRef is used to inject refernce to the host DOM
+
+  // @HostListener("mouseover") onmouseover(){
+  //   this.textHighlight("darkgray");
+  // }
+
+  // @HostListener("mouseout") onmouseout(){
+  //   this.textHighlight("");
+  // }
+
+
       private textHighlight(action:string){
         this.elem.nativeElement.style.backgroundColor =action; //elementRef grant us direct access to the host DOM element through its nativeElement prperty
   
