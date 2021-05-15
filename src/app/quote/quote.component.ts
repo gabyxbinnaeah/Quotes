362d6ef4fac82fe819a86926aef5476bc991bdd9
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-05-15 09:28:40 +0300

    Adds property quotes and specify it will be of an array type Quote, defines each quote using new keyword and passes mandatory blueprint properties

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,11 +8,11 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
     quotes:Quote[]=[
-      {id:1,name:'The greatness of any nation lies in its fidelity to the constitution and adherence to the rule of law and above all respect to God',author:'Gabriel',upVote:0,downVote:0},
-      {id:2,name:'Lightning makes no sound until it strikes.',author:'Martin Luther King Jr',upVote:0,downVote:0},
-      {id:3,name:'Aman is just but the product of his thoughtswhat he thinks he becomes ',author:'Gabriel',upVote:0,downVote:0},
-      {id:4,name:'A people without the knowledge of their past history, origin and culture is like a tree without roots.',author:'Gabriel',upVote:0,downVote:0},
-    ]
+     new Quote(1,'The greatness of any nation lies in its fidelity to the constitution and adherence to the rule of law and above all respect to God','Gabriel',0,0),
+      new Quote(2,'Lightning makes no sound until it strikes.','Martin Luther King Jr',0,0),
+      new Quote(3,'Aman is just but the product of his thoughtswhat he thinks he becomes ','Gabriel',0,0),
+      new Quote(4,'A people without the knowledge of their past history, origin and culture is like a tree without roots.','Gabriel',0,0),
+    ];
   constructor() { }
 
   ngOnInit(): void {
