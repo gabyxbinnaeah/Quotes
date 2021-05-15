95f7819ffbd5f641ac2d549ba623a0204ed53fa4
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-05-15 14:30:50 +0300

    grant us direct access to host DOM through its nativeElement property

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -5,6 +5,9 @@ import { Directive, ElementRef } from '@angular/core';
 })
 export class HighlightDirective {
 
-  constructor(private elem:ElementRef) { } //elementRef is used to inject refernce to the host DOM
+  constructor(private elem:ElementRef) {  //elementRef is used to inject refernce to the host DOM
+     this.elem.nativeElement.style.backgroundColor ='lightskyblue'; //elementRef grant us direct access to the host DOM element through its nativeElement prperty
+  
+  }
 
 }
