8c5cc7eca2a590503018533e08bf82928dd7ce4b
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-05-15 13:20:56 +0300

    Adds Output decorator which defines isComplete as an EventEmitter that takes in boolean

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
@@ -10,6 +10,10 @@ export class QuoteDetailComponent implements OnInit {
 
 
   @Input() quote!:Quote; //for recieving data from parent component
+  @Output() isComplete=new EventEmitter<boolean>(); //Output decorator is used to create event emmiter
+  // quoteComplete(complete:boolean){
+  //   this.isComplete.emit(complete);//passes event to the parent component
+  // }
   constructor() { }
 
   ngOnInit(): void {
