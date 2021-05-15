501e949882d131388656d92ce8e88f994a151c0d
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-05-15 15:27:59 +0300

    Adds createdDate to blueprint class as a property

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     showDetails!: boolean;
-    constructor(public id:number, public name:string,public author:string, public upVote:number, public downVote:number) { 
+    constructor(public id:number, public name:string,public author:string, public upVote:number, public downVote:number,public createdDate:Date) { 
         this.showDetails =false;
     }
     
@@ -8,6 +8,6 @@ export class Quote {
     
     
     
-    // createdDate!:Date;
+    // ;
 
 }
