1470c59da9788eaaf1cc644a0ad7c8758373230b
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-05-15 14:59:43 +0300

    Creates function textHighLight which takes in an action and then highlits text using action

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -5,9 +5,10 @@ import { Directive, ElementRef } from '@angular/core';
 })
 export class HighlightDirective {
 
-  constructor(private elem:ElementRef) {  //elementRef is used to inject refernce to the host DOM
-     this.elem.nativeElement.style.backgroundColor ='lightskyblue'; //elementRef grant us direct access to the host DOM element through its nativeElement prperty
+  constructor(private elem:ElementRef) { } //elementRef is used to inject refernce to the host DOM
+      private textHighlight(action:string){
+        this.elem.nativeElement.style.backgroundColor =action; //elementRef grant us direct access to the host DOM element through its nativeElement prperty
   
-  }
+      }
 
 }
